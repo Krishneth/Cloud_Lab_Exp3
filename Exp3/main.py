d0dcb2629a7cc430b47ba266518692ebdd48fede
Krishneth <129945208+Krishneth@users.noreply.github.com>
2023-08-17 16:06:19 +0530

    Update main.py

--- Exp3/main.py
+++ Exp3/main.py
@@ -1,38 +1,24 @@
-# Copyright 2018 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+from flask import Flask, request, render_template
+import requests
 
-# [START gae_python38_app]
-# [START gae_python3_app]
-from flask import Flask
-
-
-# If `entrypoint` is not defined in app.yaml, App Engine will look for an app
-# called `app` in `main.py`.
 app = Flask(__name__)
+  
+@app.route('/', methods =["GET", "POST"])
+def index(): 
+    weatherData = ''
+    error = 0
+    cityName = ''
+    if request.method == "POST":       
+        cityName = request.form.get("cityName")  
+        if cityName:
+            weatherApiKey = '5dc9501ade5a84b047f172b0726ef670'
+            url = "https://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid=" + weatherApiKey
+            weatherData = requests.get(url).json()
+        else:
+            error = 1    
+    return render_template('index.html', data = weatherData, cityName = cityName, error = error)
 
-
-@app.route('/')
-def hello():
-    """Return a friendly HTTP greeting."""
-    return 'CS8711!'
-
-
-if __name__ == '__main__':
-    # This is used when running locally only. When deploying to Google App
-    # Engine, a webserver process such as Gunicorn will serve the app. You
-    # can configure startup instructions by adding `entrypoint` to app.yaml.
+if __name__ == '__main__':   
     app.run(host='127.0.0.1', port=8080, debug=True)
-# [END gae_python3_app]
-# [END gae_python38_app]
+
 
